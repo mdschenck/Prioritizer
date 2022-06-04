a76d05d3d6988dbd874b761a432f77084f3be1ad
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-06-04 14:15:20 -0400

    Adding db, schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,33 @@
--- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+DROP DATABASE IF EXISTS prioritizer_db;
 
--- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+CREATE DATABASE prioritizer_db;
+
+USE prioritizer_db;
+
+CREATE TABLE votes (
+    vote_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    vote VARCHAR(100) NOT NULL,
+    create_date DATE DEFAULT (CURRENT_DATE),
+    start_time DEFAULT NULL
+);
+
+
+
+CREATE TABLE proposals (
+  prop_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  proposal VARCHAR(100) NOT NULL,
+  vote INT NOT NULL 
+      FOREIGN KEY (votes)
+    REFERENCES votes(vote_id)
+    ON DELETE SET NULL,
+prop_votes INT DEFAULT 0
+);
+
+
+CREATE TABLE users (
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    hash varchar(30) DEFAULT NULL,
+    votes_remaining INT(1) DEFAULT 5
+);
 
