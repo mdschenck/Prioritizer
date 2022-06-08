ea65e15811b2b0c222435a99ebcef1d4e73c1a01
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-06-07 21:02:26 -0400

    Rewrite Seeds & adding MySQL commands to insert into routes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,12 @@
 INSERT INTO votes (vote)
-VALUES ("Weekend Tasks");
+VALUES ("New Application Ideas");
 
-INSERT INTO proposals (proposal)
-VALUES ("Paint House"),
-       ("Mow Lawn"),
-       ("Rake Leaves"),
-       ("Go Fishing");
+INSERT INTO proposals (proposal, vote)
+VALUES ("Time Tracker", 1),
+       ("Social Network", 2),
+       ("Graphing Calculator", 3),
+        ("Crypto Fortuneteller", 4),
+       ("Fishing Game", 5);
 
 INSERT INTO users (name)
 VALUES ("Andres"),
