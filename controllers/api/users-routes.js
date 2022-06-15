d920a47a3faea65c6cb8a8bd4c72003eff6d58fe
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-06-15 19:10:21 -0400

    vote.js changes

--- controllers/api/users-routes.js
+++ controllers/api/users-routes.js
@@ -43,22 +43,22 @@ router.post("/:name", (req, res) => {
     });
 });
 
-// update a user's vote count -1 when vote is cast / by user `id` value
-router.put("/castVote/:id", (req, res) => {
-  User.update(
-    {
-      votes_remaining: Sequelize.literal("User.votes_remaining - 1"),
-    },
-    {
-      where: {
-        id: req.params.id,
-      },
-    }
-  )
-    .then((updatedUser) => {
-      res.json(updatedUser);
-    })
-    .catch((err) => res.json(err));
-});
+// // update a user's vote count -1 when vote is cast / by user `id` value
+// router.put("/castVote/:id", (req, res) => {
+//   User.update(
+//     {
+//       votes_remaining: Sequelize.literal("User.votes_remaining - 1"),
+//     },
+//     {
+//       where: {
+//         id: req.params.id,
+//       },
+//     }
+//   )
+//     .then((updatedUser) => {
+//       res.json(updatedUser);
+//     })
+//     .catch((err) => res.json(err));
+// });
 
 module.exports = router;
--- public/js/vote.js
+++ public/js/vote.js
@@ -2,7 +2,7 @@ const castVote = async (evt) => {
   evt.preventDefault();
   const proposal = document.querySelector("#proposal-text").value;
 
-  await fetch(`/api/votes/1`, {
+  await fetch(`/api/votes/:id`, {
     method: "PUT",
     body: JSON.stringify({
       proposal,
@@ -13,4 +13,21 @@ const castVote = async (evt) => {
   document.location.replace("/votes");
 };
 
+// update a user's vote count -1 when vote is cast / by user `id` value
+router.put("/castVote/:id", (req, res) => {
+  User.update(
+    {
+      votes_remaining: Sequelize.literal("User.votes_remaining - 1"),
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => res.json(err));
+});
 document.addEventListener("submit", castVote);
