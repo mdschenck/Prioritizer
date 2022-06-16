e2d02e26e84a7ec514c13b558f52ba2d6b8e5861
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-06-16 10:13:36 -0400

    updating chart.js

--- public/js/vote.js
+++ public/js/vote.js
@@ -2,15 +2,29 @@ const castVote = async (evt) => {
   evt.preventDefault();
   const proposal = document.querySelector("#proposal-text").value;
 
-  await fetch(`/api/votes/1`, {
+  await fetch(`/api/votes`, {
     method: "PUT",
     body: JSON.stringify({
-      proposal,
+      prop_votes: Sequelize.literal("proposal.prop_votes - 1"),
     }),
     headers: { "Content-Type": "application/json" },
   });
 
   document.location.replace("/votes");
+
+  //   router.put("/castVote/:id", (req, res) => {
+  //     User.update({
+  //             votes_remaining: Sequelize.literal("User.votes_remaining - 1"),
+  //         }, {
+  //             where: {
+  //                 id: req.params.id,
+  //             },
+  //         })
+  //         .then((updatedUser) => {
+  //             res.json(updatedUser);
+  //         })
+  //         .catch((err) => res.json(err));
+  // });
 };
 
-document.addEventListener("submit", castVote);
+document.addEventListener("click", castVote);
